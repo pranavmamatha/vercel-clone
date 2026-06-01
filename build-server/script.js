43e74cd60f21c7ce917f562203cd9823faa268a5
pranavmamatha <pranavranjithkannan@gmail.com>
2026-06-01 15:13:44 +0530

    storing logs to redis

--- build-server/script.js
+++ build-server/script.js
@@ -3,7 +3,10 @@ const path = require("path")
 const fs = require("fs")
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3")
 const { lookup } = require("mime-types")
+const Redis = require('ioredis')
 require("dotenv").config()
+
+const publisher = new Redis(process.env.REDIS);
 
 const region = process.env.AWS_REGION;
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
@@ -20,26 +23,37 @@ const s3Client = new S3Client({
 
 const PROJECT_ID = process.env.PROJECT_ID
 
+function publishLog(log) {
+  publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({ log }))
+}
+
 async function init() {
   console.log("Executing script.js")
+  console.log('Build Started...')
+  publishLog('Build Started...')
   const outDirPath = path.join(__dirname, "output")
   const child = exec(`cd ${outDirPath} && npm install && npm run build`)
 
   child.stdout.on("data", function(data) {
-    console.log(data)
+    console.log(data.toString())
+    publishLog(data.toString())
   })
 
   child.stderr.on("data", function(data) {
-    console.error(data)
+    console.log('Error', data.toString())
+    publishLog(`error: ${data.toString()}`)
   })
 
   child.on("close", async function() {
     console.log("Build Completed")
+    publishLog(`Build Completed`)
     const distDirPath = path.join(__dirname, "output", "dist")
     const distDirContent = fs.readdirSync(distDirPath, { recursive: true })
-
+    console.log("starting to upoad")
+    publishLog("Starting to upload")
     for (const filePath of distDirContent) {
       console.log("Uploading", filePath)
+      publishLog(`uploading ${file}`)
       const fullPath = path.join(distDirPath, filePath)
       if (fs.lstatSync(fullPath).isDirectory()) continue;
       const relativePath = path.relative(distDirPath, fullPath)
@@ -52,9 +66,11 @@ async function init() {
       })
 
       await s3Client.send(command)
+      publishLog(`uploaded ${file}`)
       console.log("Uploaded", filePath)
     }
     console.log("Done...")
+    publishLog(`Done...`)
   })
 }
 
